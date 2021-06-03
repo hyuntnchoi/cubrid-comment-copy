debcb937776dd8488f9f2160264226798992b9a1
hyuntn0724 <hyuntn0724@gmail.com>
2021-06-03 11:42:37 +0900

    set basic directory structure and add some html files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { CommentEditorComponent } from './comment-editor/comment-editor.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    CommentEditorComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
