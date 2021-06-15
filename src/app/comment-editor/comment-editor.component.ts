40ca31fcfa8311bfefc5554fa8c2018804d03272
hyuntn0724 <hyuntn0724@gmail.com>
2021-06-16 01:38:23 +0900

    add ViewChild in makeQueries

--- src/app/comment-editor/comment-editor.component.ts
+++ src/app/comment-editor/comment-editor.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
 	selector: 'comment-editor',
@@ -6,5 +7,64 @@ import { Component } from '@angular/core';
 	styleUrls: ['./comment-editor.component.css']
 })
 export class CommentEditorComponent {
+	@ViewChild("commentEditor") commentEditor: ElementRef;
+	constructor() {}
 
+	resultQry: string;
+
+	makeQueries(): void {
+		let columns = this.commentEditor.nativeElement.getElementsByTagName("div");
+		console.log("columns?", columns);
+		let columnLen = columns.length;
+		console.log("columns length?", columnLen);
+
+		for(let i = 0; i < columnLen; i++) {
+			let curColumn = columns[i].childNodes[0];
+			console.log("current column?", curColumn);
+			let childs = curColumn.childNodes;
+			console.log("child nodes?", childs);
+
+			let exist = this.ifCommentExists(childs);
+			console.log("comment eixsts?", exist);
+
+			if(exist) {
+				// get contents
+				let table = childs[2].textContent.trim();
+				let column = childs[4].textContent.trim();
+				let comment = childs[6].textContent.trim();
+				let dataType = childs[8].textContent.trim();
+				let query = "";
+				
+				// create insert query
+				query = this.createQry(table, column, comment, dataType);
+				console.log("created qry:", query);
+
+				// add query
+				this.resultQry = this.resultQry + query;
+			} else {
+				// do nothing
+			}
+		}
+
+		console.log(this.resultQry);
+	}
+
+	ifCommentExists(divChilds: Array<string>): boolean {
+		var len = divChilds.length;
+		
+		if(len == 10) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	createQry(table: string, column: string, comment: string, dataType: string): string {
+		console.log(`creating qry : ${table}, ${column}, ${comment}, ${dataType}`);
+
+		let query = `ALTER TABLE ${table} MODIFY COLUMN ${column} ${dataType} COMMENT '${comment}';\n`;
+
+		return query;
+	}
+	
 }
