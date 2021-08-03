2edb7814aeec09c2174abb7ffc83ab6f314a7058
hyuntn0724 <hyuntn0724@gmail.com>
2021-08-04 01:53:49 +0900

    delete unnecessary test code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('cubrid-comment-copy');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('cubrid-comment-copy app is running!');
-  });
 });
